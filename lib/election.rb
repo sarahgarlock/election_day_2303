72152263c352bb360b2ce7dffe199dca8bd5f6aa
Sarah Garlock <garlock.sarah10@gmail.com>
2023-04-14 10:55:04 -0600

    almost finish votes count

--- lib/election.rb
+++ lib/election.rb
@@ -5,16 +5,24 @@ class Election
   def initialize(year)
     @year = year
     @races = []
-
   end
 
   def add_race(race)
     @races << race
   end
 
-
   def candidates
     races.flat_map(&:candidates).uniq { |candidate| [candidate.name, candidate.party] }
   end
-  
+
+  def vote_counts
+    #   undefined method `[]' for #<Candidate:0x00007fc7f42a6d40>
+    counts = Hash.new(0)
+    @races.each do |race|
+      race.votes.each do |candidate, votes|
+        counts[candidate.name] += votes
+      end
+    end
+    counts
+  end
 end
--- spec/election_spec.rb
+++ spec/election_spec.rb
@@ -43,7 +43,40 @@ RSpec.describe Election do
 
       expect(@election.candidates.map { |candidate| [candidate.name, candidate.party] }).to eq([[candidate1.name, candidate1.party], [candidate2.name, candidate2.party]])
     end
+  end
 
-    
+  describe '#count votes' do
+    it 'returns a hash with each candidates key/value' do
+      race1 = Race.new("Colorado Governer")
+      race2 = Race.new("California Governer")
+
+      candidate1 = Candidate.new(name: 'Aaron Weld', party: :independent)
+      candidate2 = Candidate.new(name: 'Erin Jones', party: :democrat)
+      candidate3 = Candidate.new(name: 'Jim Branch', party: :republican)
+      candidate4 = Candidate.new(name: 'Betty White', party: :green)
+
+      @election.add_race(race1)
+      @election.add_race(race2)
+
+      race1.register_candidate!(candidate1)
+      race1.register_candidate!(candidate3)
+
+      race2.register_candidate!(candidate2)
+      race2.register_candidate!(candidate4)
+
+      candidate1.add_vote!
+      candidate1.add_vote!
+
+      candidate2.add_vote!
+      candidate2.add_vote!
+      candidate2.add_vote!
+
+      candidate3.add_vote!
+
+      expect(@election.vote_counts).to eq({ "Aaron Weld" => 2,
+                                            "Erin Jones" => 3, 
+                                            "Jim Branch" => 1, 
+                                            "Betty White" => 0 })
+    end
   end
 end
